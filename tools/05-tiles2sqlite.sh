b5ae5b1e4677b1c797db890386fb7ad2d17fe9a5
yvecai <yvecai@gmail.com>
2013-04-28 14:41:08 +0200

    split sqlite in 4 files

--- tools/05-tiles2sqlite.sh
+++ tools/05-tiles2sqlite.sh
@@ -19,6 +19,22 @@ echo $(date)' ######################### '
 
 echo $(date)' ######################### '
 echo $(date)' Sqlite generation starting '
-${TOOLS_DIR}./tile-list-from-db.py -o ${TMP_DIR}z16.lst -Z 16
-cat ${TMP_DIR}z16.lst | sort | uniq > ${TMP_DIR}uniq_z16.lst
-nice -n19 ${TOOLS_DIR}./render2sqlite.py ${TMP_DIR}uniq_z16.lst ${PLANET_DIR}opensnowmap.org-z16.sqlitedb
+echo $(date)' Asia '
+${TOOLS_DIR}./tile-list-from-db.py -o ${TMP_DIR}list.lst -Z 16 -j ${TOOLS_DIR}geojson/Asia_Oceania_Africa.geojson
+cat ${TMP_DIR}list.lst | sort | uniq > ${TMP_DIR}uniq.lst
+nice -n19 ${TOOLS_DIR}./render2sqlite.py ${TMP_DIR}uniq.lst ${PLANET_DIR}opensnowmap.org-Asia_Oceania_Africa-z16.sqlitedb
+
+#~ echo $(date)' Americas '
+#~ ${TOOLS_DIR}./tile-list-from-db.py -o ${TMP_DIR}list.lst -Z 16 -j ${TOOLS_DIR}geojson/Americas.geojson
+#~ cat ${TMP_DIR}list.lst | sort | uniq > ${TMP_DIR}uniq.lst
+#~ nice -n19 ${TOOLS_DIR}./render2sqlite.py ${TMP_DIR}uniq.lst ${PLANET_DIR}opensnowmap.org-Americas-z16.sqlitedb
+#~ 
+#~ echo $(date)' Northern_Europe '
+#~ ${TOOLS_DIR}./tile-list-from-db.py -o ${TMP_DIR}list.lst -Z 16 -j ${TOOLS_DIR}geojson/Northern_Europe.geojson
+#~ cat ${TMP_DIR}list.lst | sort | uniq > ${TMP_DIR}uniq.lst
+#~ nice -n19 ${TOOLS_DIR}./render2sqlite.py ${TMP_DIR}uniq.lst ${PLANET_DIR}opensnowmap.org-Northern_Europe-z16.sqlitedb
+#~ 
+#~ echo $(date)' Southern Europe '
+#~ ${TOOLS_DIR}./tile-list-from-db.py -o ${TMP_DIR}list.lst -Z 16 -j ${TOOLS_DIR}geojson/Southern_Europe.geojson
+#~ cat ${TMP_DIR}list.lst | sort | uniq > ${TMP_DIR}uniq.lst
+#~ nice -n19 ${TOOLS_DIR}./render2sqlite.py ${TMP_DIR}uniq.lst ${PLANET_DIR}opensnowmap.org-Southern_Europe-z16.sqlitedb
