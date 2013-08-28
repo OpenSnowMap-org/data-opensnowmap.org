bafd14465bac2ddf5f24de6d9251e344b87c5c63
yvecai <yvecai@gmail.com>
2013-08-28 21:35:57 +0200

    Added in_site ids to ways and nodes members of route relations

--- tools/relations_down.py
+++ tools/relations_down.py
@@ -8,34 +8,15 @@ from lxml import etree
 
 conn = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
 cur = conn.cursor()
-    
-try: 
-	cur.execute("ALTER TABLE planet_osm_line ADD member_of bigint[];")
-	conn.commit()
-except:
-	conn.rollback()
-	
-cur.execute("select id, parts from planet_osm_rels WHERE array_to_string(tags, ',') like '%,route,%' ;")
-relations=cur.fetchall()
 
-i=len(relations)
-
-for relation in relations:
-	relid=relation[0]
-	for wayid in relation[1]:
-		cur.execute("update planet_osm_line set member_of= (\
-				select array_append(member_of::bigint[], %s::bigint)\
-				from planet_osm_line where osm_id = %s limit 1) \
-			where osm_id = %s;" % (relid, wayid, wayid))
-	i-=1
-	sys.stdout.write("%s \r" % (i) )
-	sys.stdout.flush()
-conn.commit()
-
+# normalize grooming tag
+#
 cur.execute("update planet_osm_line set \"piste:grooming\"= 'classic;skating' \
 			where \"piste:grooming\"= 'classic+skating';")
 conn.commit()
 
+# copy sites ids as a in_site tag for it's members
+#
 try: 
 	cur.execute("ALTER TABLE planet_osm_line ADD in_site bigint[];")
 	conn.commit()
@@ -47,34 +28,93 @@ try:
 except:
 	conn.rollback()
 
-
-cur.execute("select id, parts from planet_osm_rels WHERE array_to_string(tags, ',') like '%,site,%' ;")
+cur.execute("select id, parts from planet_osm_rels \
+			WHERE array_to_string(tags, ',') like '%,site,%' ;")
 relations=cur.fetchall()
 
 i=len(relations)
 
 for relation in relations:
 	relid=relation[0]
-	for wayid in relation[1]:
+	for memberid in relation[1]:
+		# populate in_site for points
 		cur.execute("update planet_osm_point set in_site= (\
-				select array_append(in_site::bigint[], %s::bigint)\
-				from planet_osm_line where osm_id = %s limit 1) \
-			where osm_id = %s;" % (relid, wayid, wayid))
+					select array_append(in_site::bigint[], %s::bigint)\
+					from planet_osm_line where osm_id = %s limit 1) \
+				where osm_id = %s;" % (relid, memberid, memberid))
+		# populate in_site for ways
 		cur.execute("update planet_osm_line set in_site= (\
-				select array_append(in_site::bigint[], %s::bigint)\
-				from planet_osm_line where osm_id = %s limit 1) \
-			where osm_id = %s;" % (relid, wayid, wayid))
-		wayid = -wayid
+					select array_append(in_site::bigint[], %s::bigint)\
+					from planet_osm_line where osm_id = %s limit 1) \
+				where osm_id = %s;" % (relid, memberid, memberid))
+		# populate in_site for relations
+		memberid = -memberid
 		cur.execute("update planet_osm_line set in_site= (\
-				select array_append(in_site::bigint[], %s::bigint)\
-				from planet_osm_line where osm_id = %s limit 1) \
-			where osm_id = %s;" % (relid, wayid, wayid))
+					select array_append(in_site::bigint[], %s::bigint)\
+					from planet_osm_line where osm_id = %s limit 1) \
+				where osm_id = %s;" % (relid, memberid, memberid))
+		
 		
 	i-=1
 	sys.stdout.write("%s \r" % (i) )
 	sys.stdout.flush()
 conn.commit()
 
-cur.execute("update planet_osm_line set \"piste:grooming\"= 'classic;skating' \
-			where \"piste:grooming\"= 'classic+skating';")
+# copy relations ids as a member_of tag for it's members
+#
+try: 
+	cur.execute("ALTER TABLE planet_osm_line ADD member_of bigint[];")
+	conn.commit()
+except:
+	conn.rollback()
+try: 
+	cur.execute("ALTER TABLE planet_osm_point ADD member_of bigint[];")
+	conn.commit()
+except:
+	conn.rollback()
+
+cur.execute("select id, parts from planet_osm_rels \
+			WHERE array_to_string(tags, ',') like '%,route,%' ;")
+relations=cur.fetchall()
+
+i=len(relations)
+
+for relation in relations:
+	relid=relation[0]
+	for memberid in relation[1]:
+		# populate member_of for ways
+		cur.execute("update planet_osm_line set member_of= (\
+					select array_append(member_of::bigint[], %s::bigint)\
+					from planet_osm_line where osm_id = %s limit 1) \
+				where osm_id = %s;" % (relid, memberid, memberid))
+		# populate member_of for points
+		cur.execute("update planet_osm_point set member_of= (\
+					select array_append(member_of::bigint[], %s::bigint)\
+					from planet_osm_point where osm_id = %s limit 1) \
+				where osm_id = %s;" % (relid, memberid, memberid))
+		# populate in_site for ways and nodes members of routes relations
+		relid=-relid
+		cur.execute("select in_site from planet_osm_line \
+					where osm_id = %s;" % (relid))
+		sitesids=cur.fetchall()
+		if len(sitesids) > 0:
+			sitesids=sitesids[0][0]
+			if sitesids:
+				for siteid in sitesids:
+					# populate in_site for ways members of routes relations
+					cur.execute("update planet_osm_line set in_site= (\
+								select array_append(in_site::bigint[], %s::bigint)\
+								from planet_osm_line where osm_id = %s limit 1) \
+							where osm_id = %s;" % (siteid, memberid, memberid))
+					# populate in_site for nodes members of routes relations
+					cur.execute("update planet_osm_point set in_site= (\
+								select array_append(in_site::bigint[], %s::bigint)\
+								from planet_osm_point where osm_id = %s limit 1) \
+							where osm_id = %s;" % (siteid, memberid, memberid))
+		
+	i-=1
+	sys.stdout.write("%s \r" % (i) )
+	sys.stdout.flush()
 conn.commit()
+
+
