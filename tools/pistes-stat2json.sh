93bd8b2ed85fad4346c2e95b3394e6f67cbb0363
yves <webmaster@opensnowmap.org>
2016-06-06 23:02:46 +0200

    Update count method

--- tools/pistes-stat2json.sh
+++ tools/pistes-stat2json.sh
@@ -1,27 +1,44 @@
 downhillmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"piste:type\"='downhill';" | psql -d pistes-mapnik -U mapnik -t)
+	from planet_osm_line where \"piste:type\"='downhill' and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
 	
 nordicmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"piste:type\"='nordic' and  osm_id > 0;" | psql -d pistes-mapnik -U mapnik -t)
-	
+	from planet_osm_line where \"piste:type\"='nordic' and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
+    
 aerialwaymeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"aerialway\" is not null and  osm_id > 0;" | psql -d pistes-mapnik -U mapnik -t)
+	from planet_osm_line where \"aerialway\" in ('drag_lift', 'lift', 'cable_car', 'platter', 'j-bar', 'rope_tow', 'mixed_lift', 'gondola', 't-bar', 'chair_lift', 'magic_carpet') and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
 	
 sledmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"piste:type\"='sled' and  osm_id > 0;" | psql -d pistes-mapnik -U mapnik -t)
+	from planet_osm_line where \"piste:type\"='sled' and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
 	
 hikemeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"piste:type\"='hike' and  osm_id > 0;" | psql -d pistes-mapnik -U mapnik -t)
+	from planet_osm_line where \"piste:type\"='hike' and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
 	
 skitourmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
-	from planet_osm_line where \"piste:type\"='skitour' and  osm_id > 0;" | psql -d pistes-mapnik -U mapnik -t)
+	from planet_osm_line where \"piste:type\"='skitour' and  osm_id > 0 and member_of is null;" | psql -d pistes-mapnik -U mapnik -t)
+    
+Rdownhillmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"piste:type\"='downhill' and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
 	
-downhill=$(echo "$downhillmeter / 1000" | bc)
-nordic=$(echo "$nordicmeter / 1000" | bc)
-aerialway=$(echo "$aerialwaymeter / 1000" | bc)
-sled=$(echo "$sledmeter / 1000" | bc)
-hike=$(echo "$hikemeter / 1000" | bc)
-skitour=$(echo "$skitourmeter / 1000" | bc)
+Rnordicmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"piste:type\"='nordic' and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
+    
+Raerialwaymeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"aerialway\" in ('drag_lift', 'lift', 'cable_car', 'platter', 'j-bar', 'rope_tow', 'mixed_lift', 'gondola', 't-bar', 'chair_lift', 'magic_carpet') and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
+	
+Rsledmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"piste:type\"='sled' and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
+	
+Rhikemeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"piste:type\"='hike' and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
+	
+Rskitourmeter=$(echo "select sum(st_length_spheroid(st_transform(way,4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')) 
+	from planet_osm_line where \"piste:type\"='skitour' and  osm_id < 0;" | psql -d pistes-mapnik -U mapnik -t)
+downhill=$(     echo "($downhillmeter + $Rdownhillmeter) / 1000" | bc)
+nordic=$(       echo "($nordicmeter + $Rnordicmeter) / 1000" | bc)
+aerialway=$(    echo "($aerialwaymeter + $Raerialwaymeter) / 1000" | bc)
+sled=$(         echo "($sledmeter + $Rsledmeter) / 1000" | bc)
+hike=$(         echo "($hikemeter + $Rhikemeter) / 1000" | bc)
+skitour=$(      echo "($skitourmeter + $Rskitourmeter) / 1000" | bc)
 if  [ -d "/home/admin/" ]; then
 	date=$(cat /data/Planet/data/state.txt)
 else
