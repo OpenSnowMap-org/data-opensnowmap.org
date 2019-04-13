53013fa662ce11492d150f1844a5217103bd2d2f
yves <webmaster@opensnowmap.org>
2019-04-13 06:52:57 +0200

    Imposm3 ski pistes loading and post-processing

---
+++ tools/build-relations-DB.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+
+#import pdb
+import mapnik
+import psycopg2
+import os, sys, re
+import datetime
+"""
+Offsets={
+-1439743:-1,
+-1439729:1,
+-1439727:-1,
+-1439728:1,
+-1354364:-1,
+-1354362:0,
+-1354363:1
+}
+
+Colors={
+-1482062    :   "orange",
+-1482062    :   "orange",
+-1460895    :   "",
+-1439743    :   "black",
+-1439742    :   "red",
+-1439741    :   "red",
+-1439740    :   "",
+-1439729    :   "blue",
+-1439728    :   "green",
+-1439726    :   "green",
+-1439725    :   "red",
+-1354364:"green",
+-1354362:"yellow",
+-1354363:"red"
+}
+colorList=["orange","black","red","blue","green", "yellow"]
+"""
+
+#
+def deluma(color,factor):
+    # reduce color luma by a given factor. 
+    r,g,b,a=(color.r, color.g, color.b, color.a)
+    r=int(r*(1-0.3*factor))
+    g=int(g*(1-0.59*factor))
+    b=int(b*(1-0.11*factor))
+    
+    return mapnik.Color(r,g,b,a)
+#
+def lighten(color,factor):
+    r,g,b,a=(color.r, color.g, color.b, color.a)
+    r = int((1-factor)*r + factor*255)
+    g = int((1-factor)*g + factor*255)
+    b = int((1-factor)*b + factor*255)
+    return mapnik.Color(r,g,b,a)
+    
+def isColor(str):
+    try :
+        mapnik.Color(str)
+        return True
+    except:
+        return False
+#
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+#
+
+if len(sys.argv) < 2 :
+    print 'Provide directory as argument'
+    exit(1)
+path = sys.argv[1]
+
+filenames = os.listdir(path)
+
+Offsets = {}
+
+for fn in filenames:
+    
+    fullpath=os.path.join(path,fn)
+    f=open(fullpath,'r')
+    for line in iter(f):
+        #escape comments and html
+        if len(re.findall('#',line)) == 0 and len(re.findall('<',line)) == 0 :
+            ar=line.split(';')
+            if len(ar) == 2:
+                # check osm id:
+                if is_int(ar[0]) and is_int(ar[1]) :
+                    if int(ar[1]) != 0:
+                        Offsets[int(ar[0])] = int(ar[1])
+    f.close()
+
+#~ conn = psycopg2.connect("dbname=pistes-mapnik user=mapnik")
+conn = psycopg2.connect("dbname=pistes_imposm_tmp user=mapnik")
+
+cur = conn.cursor()
+    
+#~ cur.execute("select osm_id, coalesce(color, colour) from planet_osm_line where color is not null or colour is not null")
+cur.execute("select osm_id, coalesce(tags->'color', tags->'colour') from pistes_routes where tags->'color' is not null or tags->'colour' is not null")
+colors=cur.fetchall()
+
+Colors={}
+colorList=[]
+for c in colors:
+    Colors[int(c[0])]=c[1]
+    colorList.append(c[1])
+colorList=list(set(colorList))
+
+f=open('colors.txt','w')
+for c in colorList:
+    if isColor(c):
+        color=mapnik.Color(c)
+        color=lighten(deluma(color,0.25),0.3)
+        f.write(c+' '+str(color)+'\n')
+f.close()
+
+##############
+##############
+
+f=open('colors.csv','w')
+ids = Colors.keys()
+ids.extend(Offsets.keys())
+ids=list(set(ids))
+
+for osm_id in ids:
+    offset=0
+    color=''
+    
+    if osm_id in Colors.keys():
+        c = Colors[osm_id]
+        if isColor(c):
+            color=mapnik.Color(c)
+            color=lighten(deluma(color,0.25),0.3)
+    if osm_id in Offsets.keys():
+        offset=Offsets[osm_id]
+        
+    cur.execute("""
+        UPDATE osm_pistes_route_members
+        SET 
+          nordic_route_offset = %s,
+          nordic_route_render_colour = %s
+        WHERE osm_id=%s """, (offset, str(color), osm_id))
+    cur.execute("""
+        UPDATE pistes_routes
+        SET 
+          nordic_route_offset = %s,
+          nordic_route_render_colour = %s
+        WHERE osm_id=%s """, (offset, str(color), osm_id))
+    f.write(str(osm_id)+';'+str(color)+';'+str(offset)+'\n')
+conn.commit()
+f.close()
+exit(0)
---
+++ tools/reload_pistes_imposm.sh
@@ -0,0 +1,244 @@
+H=/home/admin/
+WORK_DIR=${H}Planet/
+
+cd ${WORK_DIR}
+# This script log
+LOGFILE=${WORK_DIR}log/planet_update.log
+# Directory where the planet file is stored
+PLANET_DIR=${WORK_DIR}data/
+TMP_DIR=${WORK_DIR}tmp/
+TOOLS_DIR=${WORK_DIR}tools/
+CONFIG_DIR=${WORK_DIR}config/
+
+dropdb -U imposm --if-exists pistes_imposm_tmp
+createdb -U imposm -E UTF8 -O imposm pistes_imposm_tmp -D data_ssd
+psql -U imposm -d pistes_imposm_tmp -c "CREATE EXTENSION postgis;"
+psql -U imposm -d pistes_imposm_tmp -c "CREATE EXTENSION hstore;" # only required for hstore support
+echo "ALTER USER imposm WITH PASSWORD 'imposm';" |psql -U imposm -d pistes_imposm_tmp
+
+readonly PG_CONNECT="postgis://imposm:imposm@localhost/pistes_imposm_tmp"
+
+osmconvert ${PLANET_DIR}planet_pistes.osm -o=${PLANET_DIR}planet_pistes.osm.pbf
+
+readonly inputpbf=${PLANET_DIR}/planet_pistes.osm.pbf
+#~ readonly inputpbf=/nvme-data/data/europe-latest.osm.pbf
+readonly mappingfile=${CONFIG_DIR}pistes.yml
+
+echo "$(date) - importing: $inputpbf "
+mkdir -p /home/admin/SSD/imposm_cache_pistes
+
+/home/yves/DEV/go/bin/./imposm3 import \
+    -quiet \
+    -mapping $mappingfile \
+    -read $inputpbf \
+    -write \
+    -optimize \
+    -overwritecache \
+    -diff -cachedir "/home/admin/SSD/imposm_cache_pistes" -diffdir "/home/admin/SSD/imposm_cache_pistes" \
+    -deployproduction \
+    -connection $PG_CONNECT
+#Fix imported geometries, otherwise we have issue with 
+#collection of polygons at the output of ST_LineMerge
+echo "UPDATE osm_pistes_route_members ..."
+echo "UPDATE osm_pistes_route_members
+        SET geometry=ST_ExteriorRing(geometry)
+        WHERE ST_GeometryType(osm_pistes_route_members.geometry)='ST_Polygon'
+    ;" |psql -U imposm -d pistes_imposm_tmp
+echo "CREATE TABLE pistes_routes ..."
+echo "CREATE TABLE pistes_routes AS (
+    SELECT 
+        osm_pistes_routes.osm_id as osm_id,
+        osm_pistes_routes.tags as tags,
+        string_agg(distinct osm_pistes_routes.name,';') as name,
+        string_agg(distinct osm_pistes_routes.piste_type,';') as piste_type,
+        string_agg(distinct osm_pistes_routes.grooming,';') as grooming,
+        bool_and(osm_pistes_routes.gladed) as gladed,
+        bool_and(osm_pistes_routes.patrolled) as patrolled,
+        bit_and(osm_pistes_routes.oneway) as oneway,
+        string_agg(distinct osm_pistes_routes.piste_name,';') as piste_name,
+        string_agg(distinct osm_pistes_routes.piste_ref,';') as piste_ref,
+        string_agg(distinct osm_pistes_routes.ref,';') as ref,
+        string_agg(distinct osm_pistes_routes.color,';') as color,
+        string_agg(distinct osm_pistes_routes.colour,';') as colour,
+        string_agg(distinct osm_pistes_routes.color,';') as nordic_route_render_colour,
+        0 as nordic_route_offset,
+        string_agg(distinct osm_pistes_routes.difficulty,';') as difficulty,
+        bool_and(osm_pistes_routes.lit1) as lit1,
+        bool_and(osm_pistes_routes.lit2) as lit2,
+        bool_and(osm_pistes_routes.abandoned1) as abandoned1,
+        bool_and(osm_pistes_routes.abandoned2) as abandoned2,
+        ST_LineMerge(
+            ST_CollectionExtract(
+                ST_Collect(osm_pistes_route_members.geometry)
+                ,2)
+        )::geometry(Geometry, 3857) AS geometry
+    FROM 
+    osm_pistes_routes
+    JOIN
+    osm_pistes_route_members ON (osm_pistes_routes.osm_id=osm_pistes_route_members.osm_id)
+    GROUP BY osm_pistes_routes.osm_id, osm_pistes_routes.tags
+    );" |psql -U imposm -d pistes_imposm_tmp
+#~ -- Add an index to the bbox column.
+echo "CREATE INDEX idx_routes_geom ON pistes_routes USING gist (geometry);" |psql -U imposm -d pistes_imposm_tmp
+#~ -- Cluster table by geographical location.
+echo "CLUSTER pistes_routes USING idx_routes_geom;" |psql -U imposm -d pistes_imposm_tmp
+echo "ANALYSE pistes_routes;" |psql -U imposm -d pistes_imposm_tmp
+#~ Rapide <2s
+#~ EXPLAIN ANALYSE SELECT st_AsBinary("geometry") AS geom
+    #~ FROM test_c
+    #~ WHERE "geometry" && st_SetSRID('BOX3D(715000. 5920000.,725000. 5930000.)'::box3d,3857);
+#~ Planning time: 0.135 ms
+#~ Execution time: 0.311 ms
+#~ Et puis de temps en temps:
+#~ echo "REFRESH MATERIALIZED VIEW pistes_routes;" |psql -U imposm -d imposm
+echo "CREATE TABLE pistes_sites ..."
+echo "CREATE TABLE pistes_sites AS (
+    SELECT 
+        osm_pistes_sites.osm_id as osm_id,
+        osm_pistes_sites.name as name,
+    
+        array_to_string(array_agg(distinct osm_pistes_ways.piste_type::text) 
+        || array_agg(distinct pistes_routes.piste_type::text)
+        || array_agg(distinct osm_pistes_area.piste_type::text)
+        || array_agg(distinct osm_sport_ways.piste_type::text)
+        || array_agg(distinct osm_sport_nodes.piste_type::text)
+        ,';') 
+        AS members_types,
+    
+        ST_ConvexHull(ST_Collect(
+        ARRAY[
+        ST_Collect(osm_pistes_ways.geometry),
+        ST_Collect(pistes_routes.geometry),
+        ST_Collect(osm_pistes_area.geometry),
+        ST_Collect(osm_sport_ways.geometry),
+        ST_Collect(osm_sport_nodes.geometry)
+        ]
+        ))::geometry(Geometry, 3857) 
+        AS geometry
+    
+    FROM 
+        osm_pistes_sites
+    LEFT JOIN osm_pistes_site_members 
+        ON (osm_pistes_sites.osm_id=osm_pistes_site_members.osm_id)
+    LEFT JOIN osm_pistes_ways 
+        ON (osm_pistes_ways.osm_id=osm_pistes_site_members.member)
+    LEFT JOIN pistes_routes 
+        ON (pistes_routes.osm_id=-osm_pistes_site_members.member)
+    LEFT JOIN osm_pistes_area 
+        ON (osm_pistes_area.osm_id=osm_pistes_site_members.member)
+    LEFT JOIN osm_sport_ways 
+        ON (osm_sport_ways.osm_id=osm_pistes_site_members.member)
+    LEFT JOIN osm_sport_nodes 
+        ON (osm_sport_nodes.osm_id=osm_pistes_site_members.member)
+    GROUP BY osm_pistes_sites.osm_id, osm_pistes_sites.name
+    );" |psql -U imposm -d pistes_imposm_tmp
+
+echo "CREATE INDEX idx_sites_geom ON pistes_sites USING gist (geometry);" |psql -U imposm -d pistes_imposm_tmp
+echo "CLUSTER pistes_sites USING idx_sites_geom;" |psql -U imposm -d pistes_imposm_tmp
+echo "ANALYSE pistes_sites;" |psql -U imposm -d pistes_imposm_tmp
+
+# landuse_ressorts
+echo "CREATE TABLE landuse_resorts ..."
+echo "CREATE TABLE landuse_resorts AS SELECT * FROM osm_resorts;
+
+ALTER TABLE landuse_resorts ADD members_types text;
+
+UPDATE landuse_resorts SET members_types = concat_ws(
+    ';', 
+    members_types,
+    (SELECT string_agg(distinct pistes_routes.piste_type::text, ';')
+        FROM pistes_routes
+        WHERE ST_Intersects(pistes_routes.geometry,landuse_resorts.geometry)
+    )
+);
+UPDATE landuse_resorts SET members_types = concat_ws(
+    ';',
+    members_types,
+    (SELECT string_agg(distinct osm_pistes_ways.piste_type::text, ';')
+        FROM osm_pistes_ways
+        WHERE ST_Intersects(osm_pistes_ways.geometry,landuse_resorts.geometry)
+    )
+);
+UPDATE landuse_resorts SET members_types = concat_ws(
+    ';',
+    members_types,
+    (SELECT string_agg(distinct osm_pistes_area.piste_type::text, ';')
+        FROM osm_pistes_area
+        WHERE ST_Intersects(osm_pistes_area.geometry,landuse_resorts.geometry)
+    )
+);
+UPDATE landuse_resorts SET members_types = concat_ws(
+    ';',
+    members_types,
+    (SELECT string_agg(distinct osm_sport_ways.piste_type::text, ';')
+        FROM osm_sport_ways
+        WHERE ST_Intersects(osm_sport_ways.geometry,landuse_resorts.geometry)
+    )
+);
+UPDATE landuse_resorts SET members_types = concat_ws(
+    ';',
+    members_types,
+    (SELECT string_agg(distinct osm_sport_nodes.piste_type::text, ';')
+        FROM osm_sport_nodes
+        WHERE ST_Intersects(osm_sport_nodes.geometry,landuse_resorts.geometry)
+    )
+);" |psql -U imposm -d pistes_imposm_tmp
+# Faudrait aussi s'assurer d'avoir plus de 3 ways ...
+#~ SELECT 363196 => autant que de landusages
+echo "CREATE INDEX idx_landuse_resorts_geom ON landuse_resorts USING gist (geometry);" |psql -U imposm -d pistes_imposm_tmp
+echo "CLUSTER landuse_resorts USING idx_landuse_resorts_geom;" |psql -U imposm -d pistes_imposm_tmp
+echo "ANALYSE landuse_resorts;" |psql -U imposm -d pistes_imposm_tmp
+
+echo "ALTER TABLE osm_pistes_route_members ..."
+echo "ALTER TABLE osm_pistes_route_members
+ADD COLUMN nordic_route_offset integer DEFAULT 0,
+ADD COLUMN nordic_route_colour text DEFAULT '',
+ADD COLUMN nordic_route_length integer DEFAULT 1000000,
+ADD COLUMN nordic_route_render_colour text DEFAULT '',
+ADD COLUMN direction_to_route integer DEFAULT 0;
+" |psql -U imposm -d pistes_imposm_tmp
+echo "ALTER TABLE osm_pistes_ways ..."
+echo "ALTER TABLE osm_pistes_ways
+ADD COLUMN nordic_route_offset integer DEFAULT 0,
+ADD COLUMN nordic_route_colour text DEFAULT '',
+ADD COLUMN nordic_route_length integer DEFAULT 1000000,
+ADD COLUMN nordic_route_render_colour text DEFAULT '',
+ADD COLUMN direction_to_route integer DEFAULT 0;
+" |psql -U imposm -d pistes_imposm_tmp
+echo "UPDATE osm_pistes_route_members ..."
+echo "UPDATE osm_pistes_route_members
+SET 
+  nordic_route_offset = 0,
+  nordic_route_colour = (SELECT coalesce(osm_pistes_routes.colour,osm_pistes_routes.color)
+                            FROM osm_pistes_routes 
+                            WHERE osm_pistes_routes.osm_id=osm_pistes_route_members.osm_id),
+  nordic_route_render_colour = (SELECT coalesce(osm_pistes_routes.colour,osm_pistes_routes.color)
+                            FROM osm_pistes_routes 
+                            WHERE osm_pistes_routes.osm_id=osm_pistes_route_members.osm_id),
+  direction_to_route = (SELECT 
+                          CASE WHEN
+                            ST_IsEmpty(
+                              ST_GeometryN(
+                                ST_SharedPaths(pistes_routes.geometry,osm_pistes_route_members.geometry),
+                                 1)
+                            )
+                            THEN -1
+                            ELSE 1
+                          END
+                        FROM pistes_routes
+                        WHERE pistes_routes.osm_id=osm_pistes_route_members.osm_id
+                        AND ST_GeometryType(osm_pistes_route_members.geometry) in ('ST_MultiLineString', 'ST_LineString')
+                        AND ST_GeometryType(pistes_routes.geometry) in ('ST_MultiLineString', 'ST_LineString')
+                            ),
+  nordic_route_length=(SELECT ST_Length(pistes_routes.geometry) 
+                            FROM pistes_routes
+                            WHERE pistes_routes.osm_id=osm_pistes_route_members.osm_id)
+                            ;
+" |psql -U imposm -d pistes_imposm_tmp
+echo "build-relations-DB ..."
+cd ${WORK_DIR}
+./build-relations-DB.py /home/admin/mapnik/offset_lists/
+
+dropdb --if-exists -U mapnik pistes_imposm
+createdb -U mapnik -T pistes_imposm_tmp pistes_imposm
+
